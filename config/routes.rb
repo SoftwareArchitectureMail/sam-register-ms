636b5a282cec361d377614f825469e475b4490cf
Steven Bustos Beltran <jsbustosb@unal.edu.co>
2017-11-14 09:04:59 -0500

    files

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :users
+  resources :places
+  wash_out :check_user
+  post 'users/login', to: 'users#login'
+  get 'user/:username', to: 'users#show_username'
+  get "test", to: 'users#test'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.orgroutil
+end
